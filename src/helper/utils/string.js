55c437734147991c795fbbd3a36d09773eb16614
Mopecat <liuyangdiv@163.com>
2018-11-16 10:58:47 +0800

    当参数中有对象的时候转换有误

--- src/helper/utils/string.js
+++ src/helper/utils/string.js
@@ -2,6 +2,9 @@ export default {
   queryString(url, query) {
     let str = []
     for (let key in query) {
+      if(typeof query[key] === 'object'){
+        query[key] = JSON.stringify(query[key]);
+      }
       str.push(key + '=' + query[key])
     }
     let paramStr = str.join('&')
