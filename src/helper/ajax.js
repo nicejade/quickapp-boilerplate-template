1576bb3f836d2816e8a83d7cb9b63fbf0bf532d5
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-01-18 13:17:37 +0800

    :sparkles: optimize data requests

--- src/helper/ajax.js
+++ src/helper/ajax.js
@@ -3,31 +3,39 @@
 import $fetch from '@system.fetch'
 import $utils from './utils'
 
+Promise.prototype.finally = function(callback) {
+  const P = this.constructor
+  return this.then(
+    value => P.resolve(callback()).then(() => value),
+    reason =>
+      P.resolve(callback()).then(() => {
+        throw reason
+      })
+  )
+}
+
 function requestHandle(params) {
   return new Promise((resolve, reject) => {
-    $fetch.fetch({
-      url: params.url,
-      method: params.method,
-      data: params.data,
-      success: data => {
-        /* @desc: 优化！存储请求返回的时间，以保证可能需要使用时间的正确性; */
-        $utils.setCurrentTime(data.headers.Date)
-
-        const serverResponse = JSON.parse(data.data)
-        if (serverResponse.success) {
-          resolve(serverResponse.value)
-        } else {
-          resolve(serverResponse.message)
-        }
-      },
-      fail: (data, code) => {
+    $fetch
+      .fetch({
+        url: params.url,
+        method: params.method,
+        data: params.data
+      })
+      .then(response => {
+        const result = response.data
+        const content = JSON.parse(result.data)
+        /* @desc: 可跟具体不同业务接口数据，返回你所需要的部分，使得使用尽可能便捷 */
+        content.success ? resolve(content.value) : resolve(content.message)
+      })
+      .catch((error, code) => {
         console.log(`🐛 request fail, code = ${code}`)
-        reject(data)
-      },
-      complete: data => {
-        reject(data)
-      }
-    })
+        reject(error)
+      })
+      .finally(() => {
+        console.log(`✔️ request @${params.url} has been completed.`)
+        resolve()
+      })
   })
 }
 
